ce33a816932a651d6d2e80e5db671031c67d9f19
Henrique Feijó <henrique.paim01@edu.pucrs.br>
2024-07-03 20:56:21 -0300

    Logs adicionados

--- imgRead.c
+++ imgRead.c
@@ -1,134 +1,127 @@
-/*
-Author: Hardik Shah
-Email: hardik05@gmail.com
-Web: http://hardik05.wordpress.com
-*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-//a vulnerable c program to explain common vulnerability types
-//fuzz with AFL
-
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
-
-struct Image
-{
-	char header[4];
-	int width;
-	int height;
-	char data[10];
+struct Image {
+    char header[4];
+    unsigned int width;
+    unsigned int height;
+    char data[10];
 };
 
-void stack_operation(){
-	char buff[0x1000];
-	while(1){
-		stack_operation();
-	}
+int ProcessImage(char* filename) {
+    FILE *fp;
+    struct Image img;
+
+    fp = fopen(filename, "r");
+    if (fp == NULL) {
+        printf("\nCan't open file or file doesn't exist.\r\n");
+        exit(0);
+    }
+
+    while (fread(&img, sizeof(img), 1, fp) > 0) {
+        printf("\n\tHeader\twidth\theight\tdata\t\r\n");
+        printf("\n\t%s\t%d\t%d\t%s\r\n", img.header, img.width, img.height, img.data);
+
+        unsigned int size1 = img.width + img.height;
+        if (size1 > 0xFFFF) {
+            printf("Error! Large value of size1 detected!\n");
+            return 0;
+        }
+
+        char* buff1 = (char*)malloc(size1);
+        if (buff1 == NULL) {
+            printf("Error! Buffer allocation failed!\n");
+            return 0;
+        }
+        if (sizeof(img.data) > size1) {
+            printf("Error! Not enough memory to copy data\n");
+            return 0;
+        }
+
+        memcpy(buff1, img.data, sizeof(img.data));
+        free(buff1);
+        buff1 = NULL;
+
+        if (size1 / 2 == 0) {
+            free(buff1);
+        } else {
+            if (size1 / 3 == 0) {
+                if (buff1 == NULL) {
+                    printf("Error! Buffer is already freed!\n");
+                    return 0;
+                } else {
+                    buff1[0] = 'a';
+                }
+            }
+        }
+
+        unsigned int size2 = img.width - img.height + 100;
+        char* buff2 = (char*)malloc(size2);
+        if (buff2 == NULL) {
+            printf("Error! Memory allocation failed!\n");
+            return 0;
+        }
+        if (sizeof(img.data) > size2) {
+            printf("Error! Not enough memory to copy data!\n");
+            return 0;
+        }
+
+        memcpy(buff2, img.data, sizeof(img.data));
+
+        if (img.height == 0) {
+            printf("Error! Image Height is zero!\n");
+            return 0;
+        }
+
+        int size3 = img.width / img.height;
+        char buff3[10];
+        char* buff4 = (char*)malloc(size3);
+        if (buff4 == NULL) {
+            printf("Error! Memory allocation failed.\n");
+            return 0;
+        }
+        if (sizeof(img.data) > size3) {
+            printf("Error! Not enough memory to copy data!\n");
+            return 0;
+        }
+        memcpy(buff4, img.data, sizeof(img.data));
+
+        if (size3 > 9) {
+            printf("Error! Memory access beyond allowed size detected!\n");
+            return 0;
+        } else {
+            char OOBR = buff3[size3];
+            buff3[size3] = 'c';
+        }
+
+        int size4 = size3 + 100;
+        if (size4 > size3) {
+            printf("Error! Memory access beyond allowed size detected!\n");
+            return 0;
+        } else {
+            char OOBR_heap = buff4[size4];
+            buff4[size4] = 'c';
+        }
+
+        if (size3 > 10) {
+            free(buff4);
+            buff4 = 0;
+        } else {
+            free(buff4);
+        }
+
+        free(buff2);
+    }
+    fclose(fp);
+    return 0;
 }
 
-int ProcessImage(char* filename){
-	FILE *fp;
-	struct Image img;
-
-	fp = fopen(filename,"r");            //Statement   1
-
-	if(fp == NULL)
-	{
-		printf("\nCan't open file or file doesn't exist.\r\n");
-		exit(0);
-	}
-
-
-	while(fread(&img,sizeof(img),1,fp)>0)
-	{
-		//if(strcmp(img.header,"IMG")==0)
-		//{
-			printf("\n\tHeader\twidth\theight\tdata\t\r\n");
-
-			printf("\n\t%s\t%d\t%d\t%s\r\n",img.header,img.width,img.height,img.data);
-
-
-			//integer overflow 0x7FFFFFFF+1=0
-			//0x7FFFFFFF+2 = 1
-			//will cause very large/small memory allocation.
-			int size1 = img.width + img.height;
-			char* buff1=(char*)malloc(size1);
-
-			//heap buffer overflow
-			memcpy(buff1,img.data,sizeof(img.data));
-			free(buff1);
-			//double free	
-			if (size1/2==0){
-				free(buff1);
-			}
-			else{
-				//use after free
-				if(size1/3 == 0){
-					buff1[0]='a';
-				}
-			}
-
-
-			//integer underflow 0-1=-1
-			//negative so will cause very large memory allocation
-			int size2 = img.width - img.height+100;
-			//printf("Size1:%d",size1);
-			char* buff2=(char*)malloc(size2);
-
-			//heap buffer overflow
-			memcpy(buff2,img.data,sizeof(img.data));
-
-			//divide by zero
-			int size3= img.width/img.height;
-			//printf("Size2:%d",size3);
-
-			char buff3[10];
-			char* buff4 =(char*)malloc(size3);
-			memcpy(buff4,img.data,sizeof(img.data));
-
-			//OOBR read bytes past stack/heap buffer
-			char OOBR = buff3[size3];
-			char OOBR_heap = buff4[size3];
-
-			//OOBW write bytes past stack/heap buffer
-			buff3[size3]='c';
-			buff4[size3]='c';
-
-			if(size3>10){
-				//memory leak here
-				buff4=0;
-			}
-			else{
-				free(buff4);
-			}
-			int size4 = img.width * img.height;
-			if(size4/2==0){
-				//stack exhaustion here
-				stack_operation();
-			}
-			else{
-				//heap exhaustion here
-				char *buff5;
-				do{
-				buff5 = (char*)malloc(size4);
-				}while(buff5);
-			}
-			free(buff2);
-		//}
-		//else
-		//	printf("invalid header\r\n");
-
-	}
-	fclose(fp);
-	return 0;
-}
-
-int main(int argc,char **argv)
-{
-	if (argc < 2) {
-    		fprintf(stderr, "no input file\n");
-    		exit(-1);
-  	}
-	ProcessImage(argv[1]);
-	return 0;
+int main(int argc, char **argv) {
+    if (argc < 2) {
+        fprintf(stderr, "no input file\n");
+        exit(-1);
+    }
+    ProcessImage(argv[1]);
+    return 0;
 }
